79c242a552d41c2c37077d6f3bfc56d6f0c9ad6d
Smerly <69550699+Smerly@users.noreply.github.com>
2020-09-21 15:24:27 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,3 @@
-#def get_animal(get_sound): 
-#    if get_sound == "meow":
-#        print("cat")
-#
-#    elif get_sound == "woof":
-#       print("dog")
-#
-#   elif get_sound == "quack":
-#        print("duck")
-#
-#    else:
-#        print("I don't recognize that animal sound")
-#    return get_sound
-
-
-#get_animal(input("insert a sound"))
 
 
 from random import choice
